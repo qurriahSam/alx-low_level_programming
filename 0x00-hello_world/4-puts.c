c86b320299d98e4e5aa41a2eb68d2a4e4c045d17
qurriahSam <mugirethe1@gmail.com>
2023-03-19 11:09:20 -0700

    remove trailing whitspace and add comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/* Prints sentence: Programming is like building a multilingual puzzle */
-
-
-int main(void) 
+/**
+ * main -Entry point uses the puts function
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
