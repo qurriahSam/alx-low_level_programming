580270052e3309a5bbac3177760dfed83dd8d5b1
qurriahSam <mugirethe1@gmail.com>
2023-03-20 15:11:09 -0700

    prints combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:This prints single digit number followed by , followed by a space
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
