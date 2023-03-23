c74474141337dcb441b183c203f0b6eb1408a4f8
qurriahSam <mugirethe1@gmail.com>
2023-03-23 06:10:15 -0700

    add more numbers file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+/**
+ * more_numbers - unction that prints 10 times the numbers,
+ * from 0 to 14 followed by a new line
+ * Return: 0;
+ */
+void more_numbers(void)
+{
+	int x;
+	int j;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
