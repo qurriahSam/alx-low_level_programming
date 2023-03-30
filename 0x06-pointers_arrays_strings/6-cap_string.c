36713c58d9afda7b4e55c77c237d33a985151ce6
qurriahSam <mugirethe1@gmail.com>
2023-03-30 07:03:39 -0700

    ft: capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize words
+ * @str: pointer
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+
+	char seps[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == seps[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (str);
+}
