6739108964d6fa63df9bdc11c7cd0f28138766b9
qurriahSam <mugirethe1@gmail.com>
2023-04-12 13:47:08 -0700

    feat: returns a pointer to the copy of a string given in parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/**
+ * *_strcop - returns a pointer coplicate of string
+ *
+ * @str: String to be coplicated
+ *
+ * Return: Null if str is null pointer otherwise
+ */
+char *_strdup(char *str)
+{
+	int length;
+	int i;
+	int j;
+	char *cop;
+
+	length = 1;
+	if (str == 0)
+	{
+		return (0);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+	cop = malloc(sizeof(char) * length);
+	if (cop == 0)
+	{
+		return (0);
+	}
+	for (j = 0; j < length; j++)
+	{
+		cop[j] = str[j];
+	}
+	return (cop);
+}
