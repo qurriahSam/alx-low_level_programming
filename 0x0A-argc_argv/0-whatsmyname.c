36327c319480fecc5cae28b9a284132668702999
qurriahSam <mugirethe1@gmail.com>
2023-07-12 07:10:34 -0700

    feat: prints function name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
